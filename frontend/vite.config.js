ed087bc51b44e95916f64f98cc04d14a48a5b7d4
Krishna <neeturaikwar914@gmail.com>
2026-02-25 23:00:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,23 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",       // << relative path ka fix
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 10000
-  }
-});
-
-
-
-
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
   base: "./",        // relative path assets ke liye
   plugins: [react()],
   build: {
