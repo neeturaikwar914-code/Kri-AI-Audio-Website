0432148e6a993e0883c10b5d0b2d05637bf100f7
Krishna <neeturaikwar914@gmail.com>
2026-02-25 21:41:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-
+import Navbar from "./components/Navbar";
 import Upload from "./components/Upload";
 
 function App() {
   return (
-    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>KRI AI Audio</h1>
-      <Upload />
-    </div>
+    <>
+      <Navbar />
+      <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+        <h1>KRI AI Audio</h1>
+        <Upload />
+      </div>
+    </>
   );
 }
 
