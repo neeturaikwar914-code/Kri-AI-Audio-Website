5c0259930bf51bbc0c380ad24cb3eb17cdcb15d5
Krishna <neeturaikwar914@gmail.com>
2026-02-25 22:58:55 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,3 +9,23 @@ export default defineConfig({
     port: 10000
   }
 });
+
+
+
+
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "./",        // relative path assets ke liye
+  plugins: [react()],
+  build: {
+    outDir: "dist"   // ensure dist folder create ho
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 10000
+  }
+});
