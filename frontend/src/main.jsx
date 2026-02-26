275ac0a21a334ced8d4d8e0d950e9295e8a8fa8b
Krishna <neeturaikwar914@gmail.com>
2026-02-26 14:48:03 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // If you have an index.css, otherwise remove this line
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./app.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-) 
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
